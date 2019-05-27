40202c0137628530385630ae51fa8e6974300400
Bruno Dias <bruno.dias@vtex.com.br>
2019-05-27 00:17:43 -0300

    Create gatsby page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+const logo =
+  'https://s.gravatar.com/avatar/e93480bce84892a336f371360e14a31d?s=80'
 
-const Header = (props) => (
-    <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
-    </header>
+const Header = () => (
+  <header id="header">
+    <span className="flex">
+      <img className="rounded-lg" src={logo} alt="" />
+    </span>
+    <h1>Bruno Dias</h1>
+    <p>Welcome to my website</p>
+  </header>
 )
 
 export default Header
